9722c1d24978441ae3444212e145b05fcfb05925
anas derk <anas.derk@gmail.com>
2025-05-21 12:28:24 +0100

    Test

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -279,8 +279,7 @@ async function postPaypalCheckoutComplete(req, res) {
 async function postStripeCheckoutComplete(req, res) {
     try {
         const result = req.body;
-        console.log(result.data.object, result.data.object.metadata)
-        if (result?.object === "event" && result?.data?.object?.payment_status == "paid" && result?.data?.object?.status === "completed" && result?.type === "checkout.session.completed") {
+        if (result?.object === "event" && result?.data?.object?.payment_status == "paid" && result?.data?.object?.status === "complete" && result?.type === "checkout.session.completed") {
             const result1 = await ordersManagmentFunctions.changeCheckoutStatusToSuccessfull(result.data.object.metadata.order_id, "en");
             res.json(result1);
             if (!result1.error) {
