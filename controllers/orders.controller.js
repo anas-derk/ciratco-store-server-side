926f62c8433e44a3da31851ffe89942debc86ad1
anas derk <anas.derk@gmail.com>
2025-05-21 12:02:11 +0100

    Test

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -219,7 +219,7 @@ async function postNewPaymentOrder(req, res) {
         }
     }
     catch (err) {
-        console.log(err);
+        console.log(err.message);
         res.status(500).json(getResponseObject(getSuitableTranslations("Internal Server Error !!", req.query.language), true, {}));
     }
 }
